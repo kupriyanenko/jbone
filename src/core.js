08615069adcc9afd2353b912d5cddd5859393f6f
kupriyanenko <a.kupriyanenko@gmail.com>
2013-11-20 23:38:07 +0200

    Performance improvements

--- src/core.js
+++ src/core.js
@@ -23,72 +23,73 @@ isObject = function(el) {
 },
 
 jBone = function(element, data) {
-    if (this instanceof jBone) {
-        return init.call(this, element, data);
-    } else {
-        return new jBone(element, data);
+    return new jBone.fn.init(element, data);
+};
+
+jBone.fn = jBone.prototype = [];
+
+jBone.fn.constructor = jBone;
+
+jBone.fn.init = function(element, data) {
+    var elements, tag, wraper, fragment;
+
+    if (typeof element === "string") {
+        // Create single DOM element
+        if (tag = rsingleTag.exec(element)) {
+            this[0] = doc.createElement(tag[1]);
+            this.length = 1;
+
+            if (isObject(data)) {
+                this.attr(data);
+            }
+
+            return this;
+        }
+        // Create DOM collection
+        else if ((tag = rquickExpr.exec(element)) && tag[1]) {
+            fragment = doc.createDocumentFragment();
+            wraper = doc.createElement("div");
+            wraper.innerHTML = element;
+            while(wraper.childNodes.length) {
+                fragment.appendChild(wraper.firstChild);
+            }
+            elements = slice.call(fragment.childNodes);
+
+            return jBone.merge(this, elements);
+        }
+        // Find DOM elements with querySelectorAll
+        else {
+            if (jBone.isElement(data)) {
+                return jBone(data).find(element);
+            }
+
+            try {
+                elements = slice.call(doc.querySelectorAll(element));
+
+                return jBone.merge(this, elements);
+            } catch (e) {
+                return this;
+            }
+        }
     }
-},
-
-init = function(element, data) {
-    var elements;
-
-    if (typeof element === "function") {
-        element();
-    } else if (element instanceof jBone) {
+    // Run function
+    else if (typeof element === "function") {
+        return element();
+    }
+    // Return jBone element as is
+    else if (element instanceof jBone) {
         return element;
-    } else if (Array.isArray(element)) {
-        elements = element.map(function(el) {
-            return getElement(el, data);
-        });
-    } else if (element) {
-        elements = getElement(element, data);
     }
-
-    if (elements instanceof jBone) {
-        return elements;
-    }
-
-    if (!elements) {
-        return this;
-    }
-
-    elements = Array.isArray(elements) ? elements : [elements];
-    jBone.merge(this, elements);
-
-    if (isObject(data) && !jBone.isElement(data)) {
-        this.attr(data);
+    // Return element wrapped by jBone
+    else {
+        element = Array.isArray(element) ? element : [element];
+        return jBone.merge(this, element);
     }
 
     return this;
-},
-
-getElement = function(element, context) {
-    var tag, wraper, fragment = doc.createDocumentFragment();
-
-    if (isString(element) && (tag = rsingleTag.exec(element))) {
-        return doc.createElement(tag[1]);
-    } else if (isString(element) && (tag = rquickExpr.exec(element)) && tag[1]) {
-        wraper = doc.createElement("div");
-        wraper.innerHTML = element;
-        while(wraper.childNodes.length) {
-            fragment.appendChild(wraper.firstChild);
-        }
-        return slice.call(fragment.childNodes);
-    } else if (isString(element)) {
-        if (jBone.isElement(context)) {
-            return jBone(context).find(element);
-        }
-
-        try {
-            return slice.call(doc.querySelectorAll(element));
-        } catch (e) {
-            return;
-        }
-    }
-
-    return element;
 };
+
+jBone.fn.init.prototype = jBone.fn;
 
 jBone.setId = function(el) {
     var jid = el.jid;
@@ -123,5 +124,3 @@ jBone._cache = {
     events: {},
     jid: 0
 };
-
-jBone.fn = jBone.prototype = [];
