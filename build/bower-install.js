3522bd4a24a30b05107dc545606bac83c4336c91
kupriyanenko <a.kupriyanenko@gmail.com>
2013-12-21 00:50:14 +0200

    Fix tabs in build scripts

--- build/bower-install.js
+++ build/bower-install.js
@@ -1,15 +1,15 @@
 var which = require("which"),
-	spawn = require("child_process").spawn;
+    spawn = require("child_process").spawn;
 
 try {
-	which.sync("bower");
+    which.sync("bower");
 } catch(error) {
-	console.error("Bower must be installed to build jBone.");
-	console.error("Please install Bower by running the following command:");
-	console.error("npm install -g bower");
-	process.exit(0);
+    console.error("Bower must be installed to build jBone.");
+    console.error("Please install Bower by running the following command:");
+    console.error("npm install -g bower");
+    process.exit(0);
 }
 
 spawn("bower", ["install"], {
-	stdio: "inherit"
+    stdio: "inherit"
 });
