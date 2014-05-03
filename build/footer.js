1134e8e80488be7d09cd9c45ba164d0c54ab7432
kupriyanenko <a.kupriyanenko@gmail.com>
2014-05-03 18:02:45 +0300

    update export for global varialble

--- build/footer.js
+++ build/footer.js
@@ -10,9 +10,9 @@ else if (typeof define === "function" && define.amd) {
     define(function() {
         return jBone;
     });
-}
 
-if (typeof win === "object" && typeof win.document === "object") {
+    win.jBone = win.$ = jBone;
+} else if (typeof win === "object" && typeof win.document === "object") {
     win.jBone = win.$ = jBone;
 }
 
